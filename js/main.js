9857c94ae28ffcf7bd0354a0cf71199451497523
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-09-26 20:32:57 -0500

    actualización página del juego

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-function init () {
+/*function init () {
     var btn1 = document.getElementById("btn1");
     var btn2 = document.getElementById("btn2");
     var btn3 = document.getElementById("btn3");
@@ -13,4 +13,50 @@ function init () {
 
 function fun1(event){
     alert("Esta función no está disponible en esta versión");
-}
+}*/
+
+window.addEventListener("load", function(){
+    document.getElementById("btn_launches").addEventListener("click", function(){
+        alert("Función NO disponible para esta versión");
+    })
+})
+
+window.addEventListener("load", function(){
+    document.getElementById("btn_free").addEventListener("click", function(){
+        alert("Función NO disponible para esta versión");
+    })
+})
+
+window.addEventListener("load", function(){
+    document.getElementById("btn_discounts").addEventListener("click", function(){
+        alert("Función NO disponible para esta versión");
+    })
+})
+
+window.addEventListener("load", function(){
+    document.getElementById("btn4_catergories").addEventListener("click", function(){
+        alert("Función NO disponible para esta versión");
+    })
+})
+
+window.addEventListener("load", function(){
+    document.getElementById("btnBuy").addEventListener("click", function(){
+        alert("Función NO disponible para esta versión");
+    })
+})
+
+window.addEventListener("load", function(){
+    document.getElementById("btonBuyGame").addEventListener("click", function(){
+        alert("Función NO disponible para esta versión");
+    })
+})
+
+
+
+
+
+
+
+
+
+
