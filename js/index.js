702ffa0a06cbea54abbd94ae48b0a670191024c9
Alejandro Quiceno Mejía <alejo.123.ks@gmail.com>
2022-10-29 18:44:20 -0500

    changes to the chat

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ const navMenu = document.querySelector(".nav-menu")
 navToggle.addEventListener("click", () =>{
     navMenu.classList.toggle("menu-bar_visible")
 })
+
+$(document).delegate(".chat-btn", "click", function() {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+    generate_message(name, 'self');
+  })
+  
+  $("#chat-circle").click(function() {    
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+  $(".chat-box-toggle").click(function() {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
